fc309fc2c6b9feb06f12a2096ae120d76230d4bf
Douglas Andrade <douglas.fandrade22@gmail.com>
2025-06-26 14:20:03 -0300

    test: add new tests for serializer validation

--- api/tests.py
+++ api/tests.py
@@ -1,57 +1,72 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Loan
+from .models import Loan, Payment
+from .serializers import LoanSerializer, PaymentSerializer
 
 
 class LoanTestCase(APITestCase):
     def setUp(self):
         self.username = "testuser"
         self.password = "testpass"
-        self.user = User.objects.create_user(username=self.username, password=self.password)
-        
-        response = self.client.post("/token/", {
-            "username": self.username,
-            "password": self.password
-        })
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password
+        )
+
+        response = self.client.post(
+            "/token/", {"username": self.username, "password": self.password}
+        )
         self.token = response.data.get("token")
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        self.default_loan = Loan.objects.create(
+            amount=1000.00,
+            interest_rate=2.5,
+            ip_address="127.1.1.1",
+            bank="Banco Casa Verde",
+            requested_at="2025-01-01",
+            client="Cristiano Ronaldo",
+            user=self.user,
+        )
+        self.default_payment = Payment.objects.create(
+            loan=self.default_loan,
+            amount=100.00,
+            payment_date="2025-02-01",
+        )
 
     def test_create_loan(self):
         data = {
-            "amount": "1000.00",
-            "interest_rate": "2.5",
+            "amount": 1000.00,
+            "interest_rate": 2.5,
             "bank": "Banco Casa Verde",
             "client": "João da Silva",
         }
         response = self.client.post("/api/loan", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Loan.objects.count(), 1)
+        self.assertEqual(Loan.objects.count(), 2)
 
     def test_create_payment(self):
-        loan = Loan.objects.create(
-            amount=1000.00,
-            interest_rate=2.5,
-            ip_address="127.1.1.1",
-            bank="Banco Casa Verde",
-            client="Cristiano Ronaldo",
-            user=self.user
-        )
+        loan = self.default_loan
         data = {
             "loan": loan.id,
-            "amount": "100.00",
-            "payment_date": "2023-10-01"
+            "amount": 100.00,
+            "payment_date": "2023-10-01",
         }
         response = self.client.post("/api/payment", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(loan.payments.count(), 1)
+        self.assertEqual(loan.payments.count(), 2)
+        self.assertEqual(response.data["loan"], loan.id)
 
     def test_list_loans(self):
+        loan_payload = LoanSerializer(self.default_loan).data
         response = self.client.get("/api/loan")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
+        self.assertEqual(response.data[0], loan_payload)
 
-    def test_list_payments(self):
-        response = self.client.get("/api/payment")
+    def test_get_payments_by_loan_id(self):
+        loan = self.default_loan
+        payment = self.default_payment
+        response = self.client.get(f"/api/payment/get_payments_by_loan_id/{loan.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
+        self.assertEqual(response.data[0], PaymentSerializer(payment).data)
--- api/views/payment_view.py
+++ api/views/payment_view.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions
 from ..models import Payment
 from ..serializers import PaymentSerializer
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,9 @@ class PaymentViewSet(viewsets.ModelViewSet):
                 "Você não pode adicionar pagamentos a empréstimos de outros usuários."
             )
         serializer.save()
+
+    @action(detail=False, methods=['get'], url_path='get_payments_by_loan_id/(?P<loan_id>[^/.]+)')
+    def get_payments_by_loan_id(self, request, loan_id=None):
+        payments = Payment.objects.filter(loan__id=loan_id, loan__user=request.user)
+        serializer = self.get_serializer(payments, many=True)
+        return Response(serializer.data)
